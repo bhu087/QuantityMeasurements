bf865bf69c04242f51872c191546ecfd7df3e4e9
bhu087 <bhu087@gmail.com>
2021-05-08 07:55:38 +0530

    [Bhushan] Add. Quantity measurement added

---
+++ QuantityMeasurement/Program.cs
@@ -0,0 +1,24 @@
+﻿/////------------------------------------------------------------------------
+////<copyright file="Program.cs" company="BridgeLabz">
+////author="Bhushan"
+////</copyright>
+////-------------------------------------------------------------------------
+namespace QuantityMeasurement
+{
+    using System;
+
+    /// <summary>
+    /// main program
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Quantity Measurement");
+        }
+    }
+}
